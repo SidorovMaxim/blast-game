a9c90c050a5b08862612319ddb2b3ca1ff0d1ebf
SidorovMaxim <maximsidorov1996@gmail.com>
2021-07-06 12:48:05 +0300

    Import files to main.js: - styles; - Pixi.js; - game field background image; - game field items images;

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('test');
+import '../main.css';
+import * as PIXI  from 'pixi.js';
+import config     from '../configs/config.json';
+import bgImg      from '../assets/backgrounds/background.png';
+import blue       from '../assets/boxes/blue.png';
+import green      from '../assets/boxes/green.png';
+import purple     from '../assets/boxes/purple.png';
+import red        from '../assets/boxes/red.png';
+import yellow     from '../assets/boxes/yellow.png';
