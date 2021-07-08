150909e0a710f4edf105cff4ab7b4abea49d4e70
SidorovMaxim <maximsidorov1996@gmail.com>
2021-07-08 16:28:01 +0300

    Remade imports and pixi aliases of "main.js"

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,43 +1,25 @@
 import '../main.css';
-import * as PIXI  from 'pixi.js';
-import animate    from './animate.js';
-import config     from '../configs/config.json';
-import bgImg      from '../assets/backgrounds/background.png';
-import blue       from '../assets/boxes/blue.png';
-import green      from '../assets/boxes/green.png';
-import purple     from '../assets/boxes/purple.png';
-import red        from '../assets/boxes/red.png';
-import yellow     from '../assets/boxes/yellow.png';
-
-
-const boxes = [blue, green, purple, red, yellow];
+import * as PIXI from 'pixi.js';
+import config from '../configs/config.json';
+import bgImg from '../assets/backgrounds/background.png';
+import { boxes, windowSizes,
+         sceneBackground, sceneHeaders,
+         Result, Moves, Score, Progress, Field }  from './gameSceneComponents.js';
 
 
 let Application     = PIXI.Application,
     Container       = PIXI.Container,
-    loader          = PIXI.Loader.shared,
-    AnimatedSprite  = PIXI.AnimatedSprite,    
-    Sprite          = PIXI.Sprite,
-    Text            = PIXI.Text,
-    TextStyle       = PIXI.TextStyle;
+    loader          = PIXI.Loader.shared;
 
-const window_width = window.innerWidth;
-const window_height = window.innerHeight;
-const window_aspect_ratio = window_width / window_height;
-
-let ratio = +((window_aspect_ratio > 1) ?
-              (window_width / config.screen.width_default) :
-              (window_height / config.screen.height_default)).toFixed(3);
 
 let app = new Application({
-  width:  (window_aspect_ratio > 1) ? (window_width) :
-          (window_height * config.screen.aspectRatio),
-  height: (window_aspect_ratio <= 1) ? (window_height) :
-          (window_width / config.screen.aspectRatio),
+  width:  (windowSizes.window_aspect_ratio > 1) ? (windowSizes.window_width) :
+          (windowSizes.window_height * config.screen.aspectRatio),
+  height: (windowSizes.window_aspect_ratio <= 1) ? (windowSizes.window_height) :
+          (windowSizes.window_width / config.screen.aspectRatio),
   backgroundColor: '0xb3b3b3',
   antialias: true
 });
-
 document.body.appendChild(app.view);
 
 
